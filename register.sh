1cca87f9cf05285fa85d39d4494ba1e7be7c3d09
zijiren233 <pyh1670605849@gmail.com>
2025-12-02 10:03:07 +0800

    fix: qemu binfmt conf sh path

--- register.sh
+++ register.sh
@@ -17,4 +17,4 @@ if [ "${1}" = "--reset" ]; then
     find /proc/sys/fs/binfmt_misc -type f -name 'qemu-*' -exec sh -c 'echo -1 > {}' \;
 fi
 
-exec qemu-binfmt-conf.sh --qemu-path "${QEMU_BIN_DIR}" "$@"
+exec ./qemu-binfmt-conf.sh --qemu-path "${QEMU_BIN_DIR}" "$@"
